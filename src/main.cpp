17fbf5cdf48ecee93c002771613f9568976d0b28
Jean Gravier <jean.gravier@ubisoft.com>
2023-09-11 19:46:19 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,186 @@
+// HelloWindowsDesktop.cpp
+// compile with: /D_UNICODE /DUNICODE /DWIN32 /D_WINDOWS /c
+
+#include <windows.h>
+#include <ScrnSave.h>
+#include <strsafe.h>
+#include <tchar.h>
+#include <wingdi.h>
+
+#include "main.h"
+#include "bitmap.h"
+
+LONG lSpeed = DEFVEL;                   // redraw speed variable
+int i = 0;
+
+CHAR szTemp[20];                        // temporary array of characters
+CHAR szRedrawSpeed[] = "Redraw Speed";  // .ini speed entry
+CHAR szIniFile[MAXFILELEN];             // .ini or registry file name
+
+HBRUSH bg = CreateSolidBrush(RGB(0xff, 0, 0));
+HBITMAP bitmap = CreateBitmap(150, 55, 3, 8, f3000ImgData);
+
+BOOL WINAPI ScreenSaverConfigureDialog(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    static HWND hSpeed;   // handle to speed scroll bar
+    static HWND hOK;      // handle to OK push button
+
+    switch (message) {
+        case WM_INITDIALOG:
+
+            // Retrieve the application name from the .rc file.
+            LoadString(hMainInstance, idsAppName, szAppName,
+                       80 * sizeof(TCHAR));
+
+            // Retrieve the .ini (or registry) file name.
+            LoadString(hMainInstance, idsIniFile, szIniFile,
+                       MAXFILELEN * sizeof(TCHAR));
+
+            // TODO: Add error checking to verify LoadString success
+            //       for both calls.
+
+            // Retrieve any redraw speed data from the registry.
+            lSpeed = GetPrivateProfileInt(szAppName, szRedrawSpeed,
+                                          DEFVEL, szIniFile);
+
+            // If the initialization file does not contain an entry
+            // for this screen saver, use the default value.
+            if (lSpeed > MAXVEL || lSpeed < MINVEL)
+                lSpeed = DEFVEL;
+
+            // Initialize the redraw speed scroll bar control.
+            hSpeed = GetDlgItem(hDlg, ID_SPEED);
+            SetScrollRange(hSpeed, SB_CTL, MINVEL, MAXVEL, FALSE);
+            SetScrollPos(hSpeed, SB_CTL, lSpeed, TRUE);
+
+            // Retrieve a handle to the OK push button control.
+            hOK = GetDlgItem(hDlg, ID_OK);
+
+            return TRUE;
+        case WM_HSCROLL:
+            // Process scroll bar input, adjusting the lSpeed
+            // value as appropriate.
+            switch (LOWORD(wParam)) {
+                case SB_PAGEUP:
+                case SB_LINEUP:
+                    --lSpeed;
+                    break;
+
+                case SB_PAGEDOWN:
+                case SB_LINEDOWN:
+                    ++lSpeed;
+                    break;
+
+                case SB_THUMBPOSITION:
+                    lSpeed = HIWORD(wParam);
+                    break;
+
+                case SB_BOTTOM:
+                    lSpeed = MINVEL;
+                    break;
+
+                case SB_TOP:
+                    lSpeed = MAXVEL;
+                    break;
+
+                case SB_THUMBTRACK:
+                case SB_ENDSCROLL:
+                    return TRUE;
+            }
+
+            if ((int) lSpeed <= MINVEL)
+                lSpeed = MINVEL;
+            if ((int) lSpeed >= MAXVEL)
+                lSpeed = MAXVEL;
+
+            SetScrollPos((HWND) lParam, SB_CTL, lSpeed, TRUE);
+            break;
+
+        case WM_COMMAND:
+            switch (LOWORD(wParam)) {
+                case ID_OK: {
+                    HRESULT hr = StringCchPrintf(szTemp, 20, "%ld", lSpeed);
+                    if (SUCCEEDED(hr))
+                        WritePrivateProfileString(szAppName, szRedrawSpeed,
+                                                  szTemp, szIniFile);
+                }
+
+                // Write the current redraw speed variable to
+                // the .ini file.
+                case ID_CANCEL:
+                    EndDialog(hDlg, LOWORD(wParam) == ID_OK);
+                    return TRUE;
+            }
+    }
+
+    return FALSE;
+}
+
+LRESULT WINAPI ScreenSaverProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    static HDC hdc;      // device-context handle
+    static RECT rc;       // RECT structure
+    static UINT uTimer;   // timer identifier
+
+    switch (message) {
+        case WM_CREATE:
+            // Retrieve the application name from the .rc file.
+            LoadString(hMainInstance, idsAppName, szAppName, 80 * sizeof(TCHAR));
+
+            // Retrieve the .ini (or registry) file name.
+            LoadString(hMainInstance, idsIniFile, szIniFile, MAXFILELEN * sizeof(TCHAR));
+
+            // TODO: Add error checking to verify LoadString success
+            //       for both calls.
+
+            // Retrieve any redraw speed data from the registry.
+            lSpeed = GetPrivateProfileInt(szAppName, szRedrawSpeed,
+                                          DEFVEL, szIniFile);
+
+            // Set a timer for the screen saver window using the
+            // redraw rate stored in Regedit.ini.
+            uTimer = SetTimer(hwnd, 1, 16, nullptr);
+            break;
+
+        case WM_ERASEBKGND:
+            // The WM_ERASEBKGND message is issued before the
+            // WM_TIMER message, allowing the screen saver to
+            // paint the background as appropriate.
+
+            hdc = GetDC(hwnd);
+            GetClientRect(hwnd, &rc);
+            FillRect(hdc, &rc, (HBRUSH) GetStockObject(BLACK_BRUSH));
+            ReleaseDC(hwnd, hdc);
+            break;
+
+        case WM_TIMER:
+            // The WM_TIMER message is issued at (lSpeed * 1000)
+            // intervals, where lSpeed == .001 seconds. This
+            // code repaints the entire desktop with a white,
+            // light gray, dark gray, or black brush each
+            // time a WM_TIMER message is issued.
+
+            hdc = GetDC(hwnd);
+            GetClientRect(hwnd, &rc);
+            FillRect(hdc, &rc, bg);
+//            if (i++ <= 4)
+//                FillRect(hdc, &rc, (HBRUSH) GetStockObject(i));
+//            else
+//                (i = 0);
+            ReleaseDC(hwnd, hdc);
+            break;
+
+        case WM_DESTROY:
+            // When the WM_DESTROY message is issued, the screen saver
+            // must destroy any of the timers that were set at WM_CREATE
+            // time.
+            if (uTimer)
+                KillTimer(hwnd, uTimer);
+            break;
+    }
+
+// DefScreenSaverProc processes any messages ignored by ScreenSaverProc.
+    return DefScreenSaverProc(hwnd, message, wParam, lParam);
+}
+
+BOOL WINAPI RegisterDialogClasses(HANDLE hInst) {
+    return TRUE;
+}
---
+++ src/main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define DLG_SCRNSAVECONFIGURE 2003
+#define MINVEL  1                 // minimum redraw speed value
+#define MAXVEL  10                // maximum redraw speed value
+#define DEFVEL  5                 // default redraw speed value
+
+#define ID_SPEED 0
+#define ID_CANCEL 1
+#define ID_OK 2
